cf95a778f30caa596fc1304abc54c9455b9395e4
Augustin ROCHE <aroche@rnf.local>
2018-06-06 09:23:53 +0200

    enregistrement geom Affleurement

--- application/views/affleurement/affleurement_form.php
+++ application/views/affleurement/affleurement_form.php
@@ -2,10 +2,10 @@
 <?= validation_errors('<div class="alert alert-warning">', '</div>') ?>
 <p>Localiser l'affleurement sur la carte par un point</p>
 <div id="map"></div>
-<form class="form form-horizontal" method="POST" action="<?= site_url('site/ajout_affleurement/'.$eg->id) ?>">
-  <?php form_hidden('geom', set_value_obj('geom', $affl)) ?>
+<form class="form form-horizontal" method="POST" action="<?= site_url('site/ajout_affleurement/'.$eg->id . (is_null($affl) ? '' : '/'.$affl->id) ) ?>">
+  <?= form_hidden('geom', set_value_obj('geom', $affl)) ?>
   <?= form_input('nom', 'Nom :', set_value_obj('nom', $affl)) ?>
-  <p>Décrire l'affleurement : Typologie de l’affleurement (chaos, bancs, lapiaz, verticalité, horizontalité, etc.)
+  <p>Décrire l’affleurement : Typologie de l’affleurement (chaos, bancs, lapiaz, verticalité, horizontalité, etc. ...)
 Nom du lieu si connu – nom de la carrière (par exemple).</p>
   <?= form_text('description', 'Description :', set_value_obj('description', $affl)) ?>
   <?= form_submit() ?>
--- resources/js/form_affl.js
+++ resources/js/form_affl.js
@@ -1,6 +1,22 @@
 $(function() {
   var map = base_map('map');
+  var ptlyr = L.circleMarker(undefined, {radius: 7, color: '#b78a31', fillColor: '#b78a31', fillOpacity: 0.5});
   addVectorLayer(map, 'carto/site_geom/' + site_id, null, function(lyr) {
     map.fitBounds(lyr.getBounds());
+    var geojsonstr = $("input[name=geom]").val();
+    if (geojsonstr) {
+      var geojson = JSON.parse(geojsonstr.replace(/&quot;/g, '"'));
+      ptlyr.setLatLng(geojson.coordinates[0].reverse());
+    } else {
+      ptlyr.setLatLng(lyr.getBounds().getCenter());
+    }
+    ptlyr.addTo(map);
   });
+
+  map.on('click', function(evt) {
+    var json = ptlyr.toGeoJSON();
+    ptlyr.setLatLng(evt.latlng);
+    $("input[name=geom]").val(JSON.stringify(json.geometry));
+  });
+
 })
