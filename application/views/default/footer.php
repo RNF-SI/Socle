b5ec710c6ff68f6dae33175a7330037dcdf7c215
Augustin Roche <augustin.roche-rnf@espaces-naturels.fr>
2019-10-03 12:50:00 +0200

    liens github footer

--- application/views/default/footer.php
+++ application/views/default/footer.php
@@ -1,14 +1,17 @@
 </div>
 <div class="container-fluid">
   <div role="footer" id="footer" class="row">
-    <div class="col-sm-4">
+    <div class="col-sm-3">
       <a href="<?=site_url('accueil/mentions_legales') ?>">Mentions légales</a>
     </div>
-    <div class="col-sm-4">
+    <div class="col-sm-3">
       <a href="http://reserves-naturelles.org/"><img src="<?= base_url('resources/images/logo_rnf.png') ?>" alt="Réserves Naturelles de France" /></a>
     </div>
-    <div class="col-sm-4">
-        <a href="mailto:<?=$this->config->item('admin_email') ?>">Contact</a>
+    <div class="col-sm-3">
+      <a href="https://github.com/RNF-SI/Socle"><span class="fab fa-github"></span> Voir la source sur Github</a>
+    </div>
+    <div class="col-sm-3">
+      <a href="https://github.com/RNF-SI/Socle/issues">Rapporter un bug</a>
     </div>
   </div>
 </div>
