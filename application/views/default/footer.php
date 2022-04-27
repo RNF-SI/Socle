1f3b7139b5634875185dd4b8ef484a2127de44db
Zacharie Moulin <zacharie.moulin@gmail.com>
2022-04-27 18:32:03 +0200

    custom page accueil

--- application/views/default/footer.php
+++ application/views/default/footer.php
@@ -1,19 +1,25 @@
 </div>
 <div class="container-fluid">
-  <div role="footer" id="footer" class="row">
-    <div class="col-sm-3">
-      <a href="<?=site_url('accueil/mentions_legales') ?>">Mentions légales</a>
+  <div role="footer" id="footer">
+    <div class="row">
+      <div class="col-sm-3">
+      </div>
+      <div class="col-sm-2">
+        <a href="<?= site_url('accueil/mentions_legales') ?>">MENTIONS LEGALES</a>
+      </div>
+      <div class="col-sm-2">
+        <a href="https://github.com/RNF-SI/Socle"><span class="fab fa-github"></span> CODE SOURCE</a>
+      </div>
+      <div class="col-sm-2">
+        <a href="https://github.com/RNF-SI/Socle/issues">RAPPORTER UN BUG</a>
+      </div>
     </div>
-    <div class="col-sm-3">
-      <a href="http://reserves-naturelles.org/"><img src="<?= base_url('resources/images/logo_rnf.png') ?>" alt="Réserves Naturelles de France" /></a>
-    </div>
-    <div class="col-sm-3">
-      <a href="https://github.com/RNF-SI/Socle"><span class="fab fa-github"></span> Voir la source sur Github</a>
-    </div>
-    <div class="col-sm-3">
-      <a href="https://github.com/RNF-SI/Socle/issues">Rapporter un bug</a>
-    </div>
+    <br>
+  <div id="copyright">
+    © Réserves Naturelles de France, 2022, licence MIT
   </div>
 </div>
+</div>
 </body>
+
 </html>
